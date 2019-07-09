ceaa43a84eef6b7a712c4f80329170c68bea4539
Cholrang.Crow- mac <cholrang.crow@gmail.com>
2019-07-09 23:04:47 +0900

    main layout - ver1.0

--- lib/main.dart
+++ lib/main.dart
@@ -25,36 +25,73 @@ class _MainState extends State<Main> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            Text('현재 위치 미세먼지'),
-            Card(
-              child: Column(
-                children: <Widget>[
-                  Row(
-                    children: <Widget>[
-                      Text('얼굴사진'),
-                      Text('80'),
-                      Text('보통'),
-                    ],
-                  ),
-                  Row(
-                    children: <Widget>[
-                      Text('얼굴사진'),
-                      Text('80'),
-                      Text('보통'),
-                    ],
-                  ),
-                ],
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                '현재 위치 미세먼지',
+                style: TextStyle(fontSize: 30),
               ),
-            ),
-            RaisedButton(
-              color: Colors.orange,
-              child: Icon(Icons.refresh, color: Colors.white),
-              onPressed: () {},
-            )
-          ],
+              SizedBox(height: 16),
+              Card(
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: <Widget>[
+                          Text('얼굴사진'),
+                          Text(
+                            '80',
+                            style: TextStyle(fontSize: 40),
+                          ),
+                          Text(
+                            '보통',
+                            style: TextStyle(fontSize: 20),
+                          ),
+                        ],
+                      ),
+                      color: Colors.yellow,
+                      padding: const EdgeInsets.all(8.0),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: <Widget>[
+                          Row(
+                            children: <Widget>[
+                              Text('사진'),
+                              Text(
+                                '29°',
+                                style: TextStyle(fontSize: 16),
+                              ),
+                            ],
+                          ),
+                          Text('습도 100%'),
+                          Text('풍속 5.7m/s'),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 16),
+              ClipRRect(
+                borderRadius: BorderRadius.circular(30),
+                child: RaisedButton(
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 15.0, horizontal: 50),
+                  color: Colors.orange,
+                  child: Icon(Icons.refresh, color: Colors.white),
+                  onPressed: () {},
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
